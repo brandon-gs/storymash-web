07c504fab052336abc4416b0ff2e7643b4e4bb25
BrandonGS <brandongs180@gmail.com>
2022-09-06 23:31:58 -0500

    Remove test from index page

--- cypress/e2e/app.cy.ts
+++ cypress/e2e/app.cy.ts
@@ -1,6 +1,9 @@
 describe("Home page", () => {
-  it("should render the h1", () => {
-    cy.visit("http://localhost:3000");
-    cy.get("h1").contains("Welcome to Next.js");
+  it("should render", () => {
+    cy.visit("http://localhost:3000/");
   });
+  // it("should render the h1", () => {
+  //   cy.visit("http://localhost:3000");
+  //   cy.get("h1").contains("Welcome to Next.js");
+  // });
 });
