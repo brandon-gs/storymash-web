c25817b1af8a4c5047b697e08a31e95b430d8d76
BrandonGS <brandongs180@gmail.com>
2022-09-30 23:20:42 -0500

    add AvatarSkeleton and InputFileButton components

--- src/core/components/index.ts
+++ src/core/components/index.ts
@@ -1,8 +1,10 @@
+export { default as AvatarWithSkeleton } from "./AvatarWithSkeleton/AvatarWithSkeleton";
 export { default as ButtonBack } from "./ButtonBack/ButtonBack";
 export { default as FormCheckbox } from "./FormCheckbox/FormCheckbox";
 export { default as FormInput } from "./FormInput/FormInput";
 export { default as useFormInput } from "./FormInput/useFormInput";
 export { default as FormInputDate } from "./FormInputDate/FormInputDate";
+export { default as FormInputFileButton } from "./FormInputFileButton/FormInputFileButton";
 export { default as Link } from "./Link/Link";
 export { default as ListValidate } from "./ListValidate/ListValidate";
 export { default as Validate } from "./ListValidate/Validate/Validate";
