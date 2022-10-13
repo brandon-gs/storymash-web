95ff7b3fe50cfd44c28f8bcffc32fbc37442420c
BrandonGS <brandongs180@gmail.com>
2022-10-13 02:16:38 -0500

    keep user cache data for 5 mins

--- src/core/services/User/userApi.ts
+++ src/core/services/User/userApi.ts
@@ -18,6 +18,7 @@ export const userApi = createApi({
         url: "/",
         method: "GET",
       }),
+      keepUnusedDataFor: 300,
       providesTags: ["User"],
     }),
     getUserAccount: builder.query<{ account: IUserAccount }, void>({
@@ -50,6 +51,7 @@ export const userApi = createApi({
         method: "PUT",
         body: body,
       }),
+      invalidatesTags: ["User"],
     }),
   }),
 });
